e8cb2569c365cd23b1668fd875f22c158ec59a22
julian <julian.guerin@gmail.com>
2020-03-29 04:03:31 +0200

    - update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,25 +13,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
+
 
         $room = Room::factory([
-            'code' => 'aaa',
-            'roles' => $faker->randomElements(Room::ROLES, 6)
+            'code' => 'bbb',
+            'roles' =>
+                [
+                    //  Room::ROLE_GAROU1,
+                    Room::ROLE_GAROU2,
+//                    Room::ROLE_SBIRE,
+                    Room::ROLE_VOYANTE,
+                    Room::ROLE_NOISEUSE,
+                    Room::ROLE_INSOMNIAQUE,
+                    Room::ROLE_VOLEUR,
+                    Room::ROLE_MACON2,
+                ],
+            'freeCards' => [
+                Room::ROLE_GAROU2,
+                Room::ROLE_MACON2,
+                Room::ROLE_INSOMNIAQUE,
+            ],
+            'step' => Room::STEP_WAITING
         ]);
         $room->save();
+
         $players = [
-            'Marine',
-            'Pol',
-            'Julian',
+            'Marine' => Room::ROLE_NOISEUSE,
+            'Pol' => Room::ROLE_VOLEUR,
+            'Julian' => Room::ROLE_VOYANTE,
+
+            //'Charlotte' => Room::ROLE_SBIRE,
+            //'Mathilde' => Room::ROLE_GAROU1,
         ];
 
-        foreach ($players as $player) {
+        foreach ($players as $player => $role) {
             $user = new User([
                 'name' => $player,
+                'original_role' => $role,
+                'current_role' => $role,
                 'room_id' => $room->id
             ]);
             $user->save();
+
         }
 
 
@@ -39,7 +62,7 @@ class DatabaseSeeder extends Seeder
             'code' => 'bbb',
             'roles' =>
                 [
-                  //  Room::ROLE_GAROU1,
+                    //  Room::ROLE_GAROU1,
                     Room::ROLE_GAROU2,
 //                    Room::ROLE_SBIRE,
                     Room::ROLE_VOYANTE,
